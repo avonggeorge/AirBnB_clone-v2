d0d7a1a9a9a50572da0b222211e62e0394ac9a70
avonggeorge <georgeavong@yahoo.com>
2024-04-05 01:54:54 +0100

    correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,62 +1,39 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
 
-# update the package lists
-sudo apt-get -y update
+# - check if Nginx is installed else install if not exists
+check=$(which nginx)
+if [ -z "$check" ]
+then
+	sudo apt install nginx
+else
+	echo "Already installed"
+fi 
 
-# install nginx
-sudo apt-get -y install nginx
+content="\
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+"
 
-# make directories for the deployment using -p to create all parent directories if not existing
-sudo mkdir -p /data/web_static/releases/test 
-sudo mkdir -p /data/web_static/shared
+# - Create Directories
+sudo mkdir -p /data/web_static/ || cd /data/web_static || sudo mkdir releases shared current;
+cd releases || mkdir test || echo "$content" > /data/web_static/releases/test/index.html
 
-# give ownership of the /data/ directory to the ubuntu user 
-sudo chown -R ubuntu /data/
+# - create a symbolic link from test to current
 
-# give group ownership of the /data/ directory to the ubuntu user
-sudo chgrp -R ubuntu /data/
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# create a test HTML file
-echo "Testing! Testing 1 , 2  1 , 2" > /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
 
-# create a symbolic link to the test HTML file
-#---'ln -s' creates a symbolic link
-#---'f' forces the symbolic link to be created if the target already exists, hence overwriting whenever target file is updated
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# sudo sed -i 's/root /var/www/html;/ alias /data/web_static/current/;/g' /etc/nginx/sites-enabled/default
+# sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# server name not specified
-# listen to port 80
-# [::] listens to IPv6 connections
-# add_header adds a header to the response indicating the server name $HOSTNAME
-# root is the root directory of the server where nginx will look for files to serve
-# index is the default file that nginx will serve if no file is specified in the URL
-# location is the path to the content to be served '/hbnb_static' path to url - https://mydomain.com/hbnb_static
-# alias is the path to the content to be served '/data/web_static/current'. check how to use 'root' and 'alias' in nginx
-# return 301 redirects the client to another URL
-# error_page 404 is the path to the custom 404 page
-# internal is used to prevent nginx from serving the page to the client
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm index.nginx-debian.html;
+sudo sed -i 's#root /var/www/html;#root /var/www/html;\n        location /hbnb_static/ {\n            alias /data/web_static/current/;\n        }#' /etc/nginx/sites-enabled/default
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm index.nginx-debian.html 8-index.html;
-    }
-
-    location /redirect_me {
-        return 301 https://twitter.com/ai_optimizer;
-    }
-
-    error_page 404 /error_404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
+# - Restart Server
 sudo service nginx restart
+
